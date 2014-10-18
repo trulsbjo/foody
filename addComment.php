8930b62ea705347a3b5c1729fb573c341bdce6fc
sindre <sindreny@gmail.com>
2014-10-18 14:18:15 +0200

    submit now working

--- addComment.php
+++ addComment.php
@@ -1,17 +1,5 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
 
-echo "###### BEFORE ######<br />";
-$myfile = fopen($commentPath, "r") or die("Unable to open file!");
-echo fread($myfile,filesize($commentPath));
-fclose($myfile);
-
-
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
-error_reporting(-1);
 $name = $_POST["name"];
 $parentPost = $_POST["parentPost"];
 $comment = $_POST["comment"];
@@ -20,7 +8,7 @@ $parentPost = str_replace(".html", "", $parentPost);
 $commentPath = "comments/".$parentPost.".xml";
 if(!file_exists($commentPath)){
     $f = fopen($commentPath, "w");
-    $startContent = "<?xml version=\"1.0\" encoding=\"UFT-8\"?>\n";
+    $startContent = "<?xml version='1.0' encoding='UTF-8'?>\n";
     $startContent .= "<comments>\n</comments>\n";
     fwrite($f, $startContent);
     fclose($f);
@@ -30,20 +18,18 @@ $myfile = fopen($commentPath, "r") or die("Unable to open file!");
 $filecontent = fread($myfile,filesize($commentPath));
 fclose($myfile);
 
-$filecontent = str_replace("</comments>", "", $filecontent);
+$filecontent = str_replace("</comments>\n", "", $filecontent);
 $filecontent .= "<post>\n";
 $filecontent .= "<name>".$name."</name>\n";
 $filecontent .= "<comment>".$comment."</comment>\n";
 $filecontent .= "<date>".date("Y-m-d")."</date>\n";
-$filecontent .= "</post>\n</comments>\n"
+$filecontent .= "</post>\n</comments>\n";
 
 $myfile = fopen($commentPath, "w") or die("Unable to open file");
 fwrite($myfile, $filecontent);
 fclose($myfile);
-
-echo "##### AFTER #####<br />";
-$myfile = fopen($commentPath, "r") or die("Unable to open file!");
-echo fread($myfile,filesize($commentPath));
-fclose($myfile);
+#header("Location: /foody/recipes/".$parentPost.".html");
+header("Location: /foody/testsubmit.html");
+exit;
 ?>
 
