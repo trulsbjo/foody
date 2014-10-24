7a537e77014fa50f692a5f272d7c22747c0b8f80
Truls Hamborg <truls.hamborg@gmail.com>
2014-10-24 13:28:56 +0200

    reCAPTCHA

--- addComment.php
+++ addComment.php
@@ -1,35 +1,51 @@
 <?php
 
-$name = $_POST["name"];
-$parentPost = $_POST["parentPost"];
-$comment = $_POST["comment"];
-$parentPost = str_replace(".html", "", $parentPost);
+ require_once('recaptcha-php-1.11/recaptchalib.php');
+ $privatekey = "6LflhfwSAAAAAHIimDpGBPE2GICytqHcdj5yjCou";
+ $resp = recaptcha_check_answer ($privatekey,
+                                 $_SERVER["REMOTE_ADDR"],
+                                 $_POST["recaptcha_challenge_field"],
+                                 $_POST["recaptcha_response_field"]);
 
-$commentPath = "comments/".$parentPost.".xml";
-if(!file_exists($commentPath)){
-    $f = fopen($commentPath, "w");
-    $startContent = "<?xml version='1.0' encoding='UTF-8'?>\n";
-    $startContent .= "<comments>\n</comments>\n";
-    fwrite($f, $startContent);
-    fclose($f);
-    chmod($commentPath, 0777);
-}
-$myfile = fopen($commentPath, "r") or die("Unable to open file!");
-$filecontent = fread($myfile,filesize($commentPath));
-fclose($myfile);
+ if (!$resp->is_valid) {
+   // What happens when the CAPTCHA was entered incorrectly
+   header("Location: http://folk.ntnu.no/trulsbjo/foody/recipes/chickenleg.html");
+   exit;
 
-$filecontent = str_replace("</comments>\n", "", $filecontent);
-$filecontent .= "<post>\n";
-$filecontent .= "<name>".$name."</name>\n";
-$filecontent .= "<comment>".$comment."</comment>\n";
-$filecontent .= "<date>".date("Y-m-d")."</date>\n";
-$filecontent .= "</post>\n</comments>\n";
+ } else {
 
-$myfile = fopen($commentPath, "w") or die("Unable to open file");
-fwrite($myfile, $filecontent);
-fclose($myfile);
-header("Location: /foody/recipes/".$parentPost.".html");
-#header("Location: /foody/testsubmit.html");
-exit;
+	$name = $_POST["name"];
+	$parentPost = $_POST["parentPost"];
+	$comment = $_POST["comment"];
+	$parentPost = str_replace(".html", "", $parentPost);
+
+	$commentPath = "comments/".$parentPost.".xml";
+	if(!file_exists($commentPath)){
+    	$f = fopen($commentPath, "w");
+    	$startContent = "<?xml version='1.0' encoding='UTF-8'?>\n";
+    	$startContent .= "<comments>\n</comments>\n";
+    	fwrite($f, $startContent);
+    	fclose($f);
+    	chmod($commentPath, 0777);
+	}
+
+	$myfile = fopen($commentPath, "r") or die("Unable to open file!");
+	$filecontent = fread($myfile,filesize($commentPath));
+	fclose($myfile);
+
+	$filecontent = str_replace("</comments>\n", "", $filecontent);
+	$filecontent .= "<post>\n";
+	$filecontent .= "<name>".$name."</name>\n";
+	$filecontent .= "<comment>".$comment."</comment>\n";
+	$filecontent .= "<date>".date("Y-m-d")."</date>\n";
+	$filecontent .= "</post>\n</comments>\n";
+
+	$myfile = fopen($commentPath, "w") or die("Unable to open file");
+	fwrite($myfile, $filecontent);
+	fclose($myfile);
+	header("Location: /foody/recipes/".$parentPost.".html");
+	#header("Location: /foody/testsubmit.html");
+	exit;
+	}
 ?>
 
